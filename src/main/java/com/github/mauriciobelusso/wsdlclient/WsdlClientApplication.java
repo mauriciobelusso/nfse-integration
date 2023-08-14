e2993cea4e2a47fd6e159a9b18106daf814a69e6
mauriciobelusso <mauriciobelusso@gmail.com>
2023-08-14 17:24:53 -0300

    First commit

---
+++ src/main/java/com/github/mauriciobelusso/wsdlclient/WsdlClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.mauriciobelusso.wsdlclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mauriciobelusso/wsdlclient/WsdlClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mauriciobelusso.wsdlclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
